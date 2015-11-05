e568d3f0c7f450da6ba22fae5a349cabc32ada75
alvaromb <amedina@apsl.net>
2015-11-05 14:13:44 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-native-navigator-wrapper
+A React Native Navigator component wrapper that implements nested navigators for both push and modal transitions.
---
+++ index.js
@@ -0,0 +1,9 @@
+import NavBarBackButton from './lib/NavBarBackButton'
+import NavBar from './lib/NavBar'
+import NavigatorWrapper from './lib/NavigatorWrapper'
+import TopNavigatorWrapper from './lib/TopNavigatorWrapper'
+
+export default NavBarBackButton
+export default NavBar
+export default NavigatorWrapper
+export default TopNavigatorWrapper
